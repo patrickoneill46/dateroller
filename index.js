86905b0b5c9bd9ca37a6006166f101b93905a7d8
Patrick O'Neill <patrick.oneill46@gmail.com>
2014-10-21 18:20:21 +0100

    Partial commit, might need to roll back. Adding roll date strategies

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 var fs = require('fs'),
-	moment = require('moment');
+	moment = require('moment'),
+	events = require('events');
 
+var eventEmitter = new events.EventEmitter();	
 
-var NUM_DAYS_BY_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var NUM_DAYS_BY_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+	BANK_HOLIDAYS = {},
+	DATE_ROLLING_STRATEGIES = {};
 
-var BANK_HOLIDAYS = {};
+function loadHolidayData(callback){
 
-if (process.env.HOLIDAY_JSON_PATH){
+	if (process.env.HOLIDAY_JSON_PATH){
 
-	fs.readFile(process.env.HOLIDAY_JSON_PATH, function(err, data){
+		fs.readFile(process.env.HOLIDAY_JSON_PATH, function(err, data){
 
-		if (err){
-			throw err
-		} else{
+			if (err){
 
-			var holidayJsonData = JSON.parse(data);
+				callback(err);
 
-			for (var market in holidayJsonData){
+			} else {
 
-				BANK_HOLIDAYS[market] = [];
+				var holidayJsonData = JSON.parse(data);
 
-				holidayJsonData[market].forEach(function(holiday){
-					
-					BANK_HOLIDAYS[market].push(new moment({
-						year: holiday.year,
-						month: holiday.month,
-						day: holiday.dayOfMonth
-					}));
-				});
+				for (var market in holidayJsonData){
+
+					BANK_HOLIDAYS[market] = [];
+
+					holidayJsonData[market].forEach(function(holiday){
+						
+						BANK_HOLIDAYS[market].push(new moment({
+							year: holiday.year,
+							month: holiday.month,
+							day: holiday.dayOfMonth
+						}));
+					});
+				}
+
+				callback();
 			}
-		}
-	});
+		});
+	}
 }
 
 function isHoliday(date, sMarket){
@@ -41,9 +50,7 @@ function isHoliday(date, sMarket){
 	sMarket = sMarket || 'UK'
 
 	return BANK_HOLIDAYS[sMarket].some(function(element){
-
 		return date.isSame(element, 'day');
-
 	});
 
 }
@@ -51,3 +58,10 @@ function isHoliday(date, sMarket){
 function isWeekDay(nDayOfWeek){
 	return [0, 6].indexOf(nDayOfWeek) === -1;
 }
+
+loadHolidayData(function(){
+	console.log('data loaded');
+});
+
+exports.BANK_HOLIDAYS = BANK_HOLIDAYS;
+
---
+++ strategies/actual.js
@@ -0,0 +1,14 @@
+//
+
+module.exports = function(dateRoller){
+
+
+	function rollDate(date){
+		return date;//no need to roll as it is actual
+	}
+
+	return {
+		rollDate: rollDate
+	}
+
+}
