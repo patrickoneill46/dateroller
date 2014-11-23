0b7a19c84944323d395b55543e5b2062d49e407a
Patrick O'Neill <patrick.oneill46@gmail.com>
2014-11-23 21:10:59 +0000

    Adding mocha test to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,23 @@ module.exports = function(grunt){
         all: {
     			src: ['./Gruntfile.js', './index.js', './test/*.js'],
         }
-  		}
+  		},
+
+      // Configure a mochaTest task
+      mochaTest: {
+        test: {
+          options: {
+            reporter: 'spec',
+            captureFile: 'test/results.txt', // Optionally capture the reporter output to a file
+            quiet: false, // Optionally suppress output to standard out (defaults to false)
+            clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+          },
+          src: ['test/*.js']
+        }
+      }
 
   	});
 
-  	grunt.registerTask('default', ['jshint:all']);
+  	grunt.registerTask('default', ['jshint:all', 'mochaTest']);
 
 };
